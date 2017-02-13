a03a2bdf9acc5240a6453f4b25a170af7d879868
Stadnichenko Oleg <oleg.stadnichenko@gmail.com>
2017-02-13 20:08:32 +0300

    Initial Commit common libraries for Masks and MakeUp

---
+++ src/main/java/ru/flightlabs/commonlib/Settings.java
@@ -0,0 +1,23 @@
+package ru.flightlabs.commonlib;
+
+public class Settings {
+
+    public static String PHOTO = "photo";
+
+    public static boolean debugMode = false;
+    public static final String DIRECTORY_SELFIE = "Masks";
+    
+    public static final String PREFS = "eselfie";
+    public static final String DEBUG_MODE = "debugMode";
+    public static final String MULTI_MODE = "multiMode";
+    public static final String PUPILS_MODE = "pupilsMode";
+    public static final String COUNTER_PHOTO = "photoCounter";
+    public static final String MODEL_PATH = "modelPath";
+    public static final String MODEL_PATH_DEFAULT = "/storage/extSdCard/sp_s2.dat";
+    public static boolean useLinear;
+    public static boolean makeUp;
+    public static boolean useKalman;
+
+    public static Class clazz;
+
+}
---
+++ src/main/java/ru/flightlabs/masks/Static.java
@@ -0,0 +1,26 @@
+package ru.flightlabs.masks;
+
+import org.opencv.core.Mat;
+
+import ru.flightlabs.commonlib.R;
+
+/**
+ * Created by sov on 06.01.2017.
+ */
+
+public class Static {
+
+    public static boolean makePhoto;
+    public static boolean makePhoto2;
+
+    public static boolean drawOrigTexture;
+    public static boolean libsLoaded;
+
+
+    public static int currentIndexEye = -1;
+    public static int newIndexEye = 0;
+
+    public static Mat glViewMatrix2;
+
+    public static final int[] resourceDetector = {R.raw.lbpcascade_frontalface, R.raw.haarcascade_frontalface_alt2, R.raw.my_detector};
+}
